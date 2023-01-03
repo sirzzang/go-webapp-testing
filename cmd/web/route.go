345b39c4e78cf7feac9ef60209a80b2495672856
sirzzang <sirzzang@naver.com>
2023-01-03 22:35:27 +0900

    initial commit

---
+++ cmd/web/route.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// register middleware
+	mux.Use(middleware.Recoverer)
+
+	// register routes
+
+	// static assets
+
+	return mux
+
+}
