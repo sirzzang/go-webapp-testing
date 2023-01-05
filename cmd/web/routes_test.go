3c9cda51e58fb6057fa902ee6e11b132442418e7
sirzzang <sirzzang@naver.com>
2023-01-05 16:14:09 +0900

    feat: add routes test

---
+++ cmd/web/routes_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+)
+
+// Test if all the routes are registered
+func Test_application_routes(t *testing.T) {
+	var registerted = []struct {
+		route  string
+		method string
+	}{
+		{"/", "GET"},
+	}
+
+	var app application
+	mux := app.routes()
+
+	chiRoutes := mux.(chi.Routes)
+
+	for _, route := range registerted {
+		// check to see if the route exists
+		if !routeExists(route.route, route.method, chiRoutes) {
+			t.Errorf("route %s is not registered", route.route)
+		}
+	}
+}
+
+func routeExists(testRoute, testMethod string, chiRoutes chi.Routes) bool {
+	found := false
+
+	_ = chi.Walk(chiRoutes, func(method, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		if strings.EqualFold(method, testMethod) && strings.EqualFold(route, testRoute) {
+			found = true
+		}
+		return nil
+	})
+
+	return found
+}
