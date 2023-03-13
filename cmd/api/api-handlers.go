97c195b05deada045ae541e81bf851544b2536fe
sirzzang <sirzzang@naver.com>
2023-03-13 20:35:55 +0900

    feat: generate tokens

--- cmd/api/api-handlers.go
+++ cmd/api/api-handlers.go
@@ -1,8 +1,71 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+	"webapp/pkg/data"
+
+	"github.com/golang-jwt/jwt/v4"
+)
 
 func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+
+	// read a json payload
+
+	// look up the user by email address
+
+	// check password
+
+	// generate tokens
+
+	// send token to user
+
+}
+
+func (app *application) generateTokenPairs(user *data.User) (TokenPairs, error) {
+	// create the token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	claims["sub"] = fmt.Sprint(user.ID)
+	claims["aud"] = app.Domain
+	claims["iss"] = app.Domain
+	if user.IsAdmin == 1 {
+		claims["admin"] = true
+	} else {
+		claims["admin"] = false
+	}
+
+	// set expiry
+	claims["exp"] = time.Now().Add(jwtTokenExpiry).Unix()
+
+	// create signed token
+	signedAccessToken, err := token.SignedString([]byte(app.JWTSecret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// create the refresh token
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	refreshTokenClaims := refreshToken.Claims.(jwt.MapClaims)
+	refreshTokenClaims["sub"] = fmt.Sprint(user.ID)
+
+	// set refresh token expiry; must be longer than jwt expiry
+	refreshTokenClaims["exp"] = time.Now().Add(refreshTokenExpiry).Unix()
+	signedRefreshToken, err := token.SignedString([]byte(app.JWTSecret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	var tokenPairs = TokenPairs{
+		Token:        signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+
+	return tokenPairs, nil
 
 }
 
--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -13,7 +13,7 @@ import (
 const jwtTokenExpiry = time.Minute * 15
 const refreshTokenExpiry = time.Hour * 24
 
-type tokenPairs struct {
+type TokenPairs struct {
 	Token        string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 }
